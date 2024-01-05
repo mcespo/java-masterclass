2471c04dcc9cdfc1784b809891508a135126b26b
Marvin Céspedes <mcespo@gmail.com>
2024-01-05 00:08:39 -0600

    basic run command

---
+++ src/com/amigoscode/Main.java
@@ -0,0 +1,8 @@
+package com.amigoscode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello java \uD83D\uDE2C");
+        System.out.println(12+12+6);
+    }
+}

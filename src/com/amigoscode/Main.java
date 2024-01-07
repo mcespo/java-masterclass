ba855b18b0a1c9946c6a57ef264bc6795fc51781
Marvin Céspedes <mcespo@gmail.com>
2024-01-07 13:35:24 -0600

    playing with averages

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,8 +1,18 @@
 package com.amigoscode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello java \uD83D\uDE2C");
-        System.out.println(12+12+6);
+        ArrayList<Integer> bills = new ArrayList<>(Arrays.asList(162,106,65,121,60,182,96,92));
+        int total = 0;
+
+        for (int value : bills) {
+            total += value;
+        }
+
+        System.out.println(total/bills.size());
     }
 }
